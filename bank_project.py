f363384e6b324cd6a901b3106d716c545b2ff00b
Viet Nguyen <130389965+JimmyNguyen11@users.noreply.github.com>
2024-03-25 00:12:50 +0700

    commited

---
+++ bank_project.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import sqlite3
+import numpy as np
+
+def log_progress(message):
+    ''' This function logs the mentioned message at a given stage of the 
+    code execution to a log file. Function returns nothing.'''
+
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
+def extract(url, table_attribs):
+    ''' The purpose of this function is to extract the required
+    information from the website and save it to a dataframe. The
+    function returns the dataframe for further processing. '''
+
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+      if row.find('td') is not None:
+          col = row.find_all('td')
+          bank_name = col[1].find_all('a')[1]['title']
+          market_cap = col[2].contents[0]
+          data_dict = {"Name": bank_name,
+                       "MC_USD_Billion": float(market_cap)}
+          df1 = pd.DataFrame(data_dict, index=[0])
+          df = pd.concat([df,df1], ignore_index=True)
+          
+    return df
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+    exchange_rate = pd.read_csv(csv_path)
+    exchange_rate = exchange_rate.set_index('Currency').to_dict()['Rate']
+    df['MC_GBP_Billion'] = [np.round(x*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
+    return df
+
+def load_to_csv(df, csv_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+
+    df.to_csv(csv_path)
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe to as a database table
+    with the provided name. Function returns nothing.'''
+
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+    
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+    
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Name", "MC_USD_Billion"]
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+csv_path = './Largest_banks_data.csv'
+exchangecsvpath = 'D:/DE_projects/bank_project/exchange_rate.csv'
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df, exchangecsvpath)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('Banks.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Executing queries')
+
+query_statement = f"SELECT * from {table_name}"
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM Largest_banks"
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT Name from Largest_banks LIMIT 15"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()    
+
+log_progress('Sever Connection closed.')
+    
+    
+    
+    
+    
+
+    
